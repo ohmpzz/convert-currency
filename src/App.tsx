65c412c800a41c5b66769d8fdd1ad0e187929789
Ohm Piromrak <ohmpzzz@gmail.com>
2024-05-07 15:52:09 +0700

    update cal

--- src/App.tsx
+++ src/App.tsx
@@ -2,31 +2,33 @@ import { useState, useEffect } from "react";
 import { Container, TextField, Typography, Stack } from "@mui/material";
 
 function App() {
-  const [from, setFrom] = useState(0);
-  const [to, setTo] = useState(40.5);
+  const [from, setFrom] = useState<number | null>(null);
+  const [to, setTo] = useState<number | null>(40.5);
   const [calculated, setCalculated] = useState<string>("0");
 
   useEffect(() => {
-    setCalculated((from * to).toFixed(2));
+    setCalculated((+(from || 0) * +(to || 0)).toFixed(2));
   }, [from, to]);
 
   return (
     <Container>
-      <Stack flexDirection={"column"} spacing={4} mt={4}>
+      <Stack flexDirection={"column"} spacing={4} mt={6} justifyContent={"center"}>
         <TextField
+          type="number"
           label="From"
           value={from}
           onChange={(e) => {
             const value = e?.target?.value;
-            setFrom(Number(value) || 0);
+            setFrom(Number(value) || null);
           }}
         />
         <TextField
+          type="number"
           label="To"
           value={to}
           onChange={(e) => {
             const value = e?.target?.value;
-            setTo(Number(value) || 0);
+            setTo(Number(value) || null);
           }}
         />
         <Typography variant="h4" textAlign={"center"}>
